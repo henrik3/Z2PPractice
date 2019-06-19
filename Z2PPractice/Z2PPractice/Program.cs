ed155d2270504d506ea3729bf7d448a93725a2b3
Henrik Hamre <henrik.hamre@ttsgroup.com>
2019-06-19 16:38:11 +0200

    Static method/classes tutorial

--- Z2PPractice/Z2PPractice/Program.cs
+++ Z2PPractice/Z2PPractice/Program.cs
@@ -6,7 +6,34 @@ namespace Z2PPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+            myCar.Color = "Blue";
+
+            Car myCar2 = new Car();
+            myCar2.Color = "Red";
+
+            Math myMath = new Math();
+            int total = Math.Add(5, 3);
+
+
+            Math myMath2 = new Math();
+            int total2 = Math.Add(6, 12);
+
+            Console.WriteLine(total);
+            Console.WriteLine(total2);
+        }
+    }
+
+    class Car
+    {
+        public string Color { get; set; }
+    }
+
+    class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
